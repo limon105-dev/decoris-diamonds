7c0954c07c8b01dbc4dd75b10c67ff85b2be4ba8
Limon ALi <limonali105@gmail.com>
2025-10-14 18:26:00 +0600

    collection page added

--- assets/js/script.js
+++ assets/js/script.js
@@ -80,13 +80,13 @@ $(document).ready(function () {
 	$(".filter-window-close-btn").click(function (event) {
 		event.stopPropagation();
 		$(".collection-filter").removeClass('active');
-		$("body").toggleClass('active');
+		$("body").removeClass('active');
 	});
 
 	$(".filter-close-btn").click(function (event) {
 		event.stopPropagation();
 		$(".collection-filter").removeClass('active');
-		$("body").toggleClass('active');
+		$("body").removeClass('active');
 	});
 	// filter sidebar js end--
 
@@ -295,3 +295,47 @@ var swiper2 = new Swiper(".product-slider", {
 	},
 });
 // product-slider js end---
+
+// price range js start---
+const rangeInput = document.querySelectorAll(".range-input input"),
+  priceInput = document.querySelectorAll(".price-input input"),
+  range = document.querySelector(".slider .progress");
+let priceGap = 1000;
+
+priceInput.forEach((input) => {
+  input.addEventListener("input", (e) => {
+    let minPrice = parseInt(priceInput[0].value),
+      maxPrice = parseInt(priceInput[1].value);
+
+    if (maxPrice - minPrice >= priceGap && maxPrice <= rangeInput[1].max) {
+      if (e.target.className === "input-min") {
+        rangeInput[0].value = minPrice;
+        range.style.left = (minPrice / rangeInput[0].max) * 100 + "%";
+      } else {
+        rangeInput[1].value = maxPrice;
+        range.style.right = 100 - (maxPrice / rangeInput[1].max) * 100 + "%";
+      }
+    }
+  });
+});
+
+rangeInput.forEach((input) => {
+  input.addEventListener("input", (e) => {
+    let minVal = parseInt(rangeInput[0].value),
+      maxVal = parseInt(rangeInput[1].value);
+
+    if (maxVal - minVal < priceGap) {
+      if (e.target.className === "range-min") {
+        rangeInput[0].value = maxVal - priceGap;
+      } else {
+        rangeInput[1].value = minVal + priceGap;
+      }
+    } else {
+      priceInput[0].value = minVal;
+      priceInput[1].value = maxVal;
+      range.style.left = (minVal / rangeInput[0].max) * 100 + "%";
+      range.style.right = 100 - (maxVal / rangeInput[1].max) * 100 + "%";
+    }
+  });
+});
+// price range js end---
